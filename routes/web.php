c144d34cb12cd2c33d595c5163b68c2a05d25641
Myron <2453302174@qq.com>
2018-06-01 13:44:17 +0800

    ali composer

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,21 @@
 */
 
 Route::get('/', function () {
+    dd('exit');
     return view('welcome');
+});
+
+Route::group(['prefix'=>'wxpay'],function(){
+    //微信支付
+    Route::get('prepay','TestController@wxPrepay');
+    //支付查询
+    Route::get('prepay-query','TestController@wxPrepayQuery');
+    //jsapi支付
+    Route::get('jspay','TestController@wxJspay');
+    //app支付
+    Route::get('app-pay','TestController@wxAppPay');
+    //支付查询
+    Route::get('query','TestController@wxQuery');
+    //退款
+    Route::get('refund','TestController@wxRefund');
 });
