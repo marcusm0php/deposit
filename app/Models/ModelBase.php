354aa4c1f364ea34b3686cfeba69bebd744eaeab
Myron <2453302174@qq.com>
2018-06-04 12:56:34 +0800

    model base

--- app/Models/ModelBase.php
+++ app/Models/ModelBase.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ModelBase extends Model
 {
     const CREATED_AT = 'create_time';
-    const UPDATED_AT = '';
+    const UPDATED_AT = 'update_time';
     protected $dateFormat = 'Y-m-d H:i:s';
     
     public function getUpdatedAtColumn() {
