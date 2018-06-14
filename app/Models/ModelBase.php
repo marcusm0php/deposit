3f694711686f793bc95a23beecc1622e3ad813a8
lz <lz@qq.com>
2018-06-14 11:22:52 +0800

    hidden important column

--- app/Models/ModelBase.php
+++ app/Models/ModelBase.php
@@ -9,5 +9,16 @@ class ModelBase extends Model
     const CREATED_AT = 'create_time';
     const UPDATED_AT = 'update_time';
     protected $dateFormat = 'Y-m-d H:i:s';
-    
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->init();
+    }
+
+    public function init()
+    {
+        $this->hidden[] = $this->primaryKey;
+    }
 }
